c2a341af2d7179c6cc493cd4e6a5276ad8a872fb
iakessel <nicolas.kesseli@stud.hslu.ch>
2019-05-06 19:07:23 +0200

    Task 4,5,6

--- BIDS.R
+++ BIDS.R
@@ -31,10 +31,77 @@ lines(soil$OC2, type="l", col="red")
 lines(soil$OC5, type="l", col="green")
 
 # Task 4
+plot(CEC1 ~ Clay1, data=soil)
+cor(soil$CEC1,soil$Clay1)
+(fit = lm(CEC1 ~ Clay1, data = soil))
+plot(CEC1 ~ Clay1, data=soil) + abline(fit)
+
+plot(CEC2 ~ Clay2, data=soil)
+cor(soil$CEC2,soil$Clay2)
+(fit = lm(CEC2 ~ Clay2, data = soil))
+plot(CEC2 ~ Clay2, data=soil) + abline(fit)
+
+plot(CEC5 ~ Clay5, data=soil)
+cor(soil$CEC5,soil$Clay5)
+(fit = lm(CEC5 ~ Clay5, data = soil))
+plot(CEC5 ~ Clay5, data=soil) + abline(fit)
+
+plot(CEC1 ~ OC1, data=soil)
+cor(soil$CEC1,soil$OC1)
+(fit = lm(CEC1 ~ OC1, data = soil))
+plot(CEC1 ~ OC1, data=soil) + abline(fit)
+
+plot(CEC2 ~ OC2, data=soil)
+cor(soil$CEC2,soil$OC2)
+(fit = lm(CEC2 ~ OC2, data = soil))
+plot(CEC2 ~ OC2, data=soil) + abline(fit)
+
+plot(CEC5 ~ OC5, data=soil)
+cor(soil$CEC5,soil$OC5)
+(fit = lm(CEC5 ~ OC5, data = soil))
+plot(CEC5 ~ OC5, data=soil) + abline(fit)
 
 # Task 5
+fit = lm(CEC1 ~ Clay1, data = soil)
+summary(fit)$r.squared
+fit2 = lm(CEC1 ~ OC1, data = soil)
+summary(fit2)$r.squared
+#OC ist der bessere Prädiktor
+
+fit3 = lm(CEC2 ~ OC2, data = soil)
+summary(fit3)$r.squared
+fit4 = lm(CEC2 ~ Clay2, data = soil)
+summary(fit4)$r.squared
+#Clay ist der bessere Prädiktor
+
+fit5 = lm(CEC5 ~ Clay5, data = soil)
+summary(fit5)$r.squared
+fit6 = lm(CEC5 ~ OC5, data = soil)
+summary(fit6)$r.squared
+#Clay ist der bessere Prädiktor
 
 # Task 6
+regression = lm(CEC1 ~ Clay1+OC1, data = soil)
+#Die lineare Gleichung lautet: y=0.06475*Clay1+2.16240*OC11+2.71960
+regression$coefficients
+summary(regression)
+
+#Predictions
+pred1 = forecast(regression, newdata=data.frame(Clay1 = soil$Clay2, OC1 = soil$OC2))
+print(pred1)
+pred2 <- forecast(regression, newdata=data.frame(Clay1 = soil$Clay5, OC1 = soil$OC5))
+print(pred2)
+
+#Berechnung R-Squared
+pred1 <- pred1$mean
+myDf <- as.data.frame(pred1)
+regression = lm(pred1 ~ Clay1+OC1, data = soil)
+summary(regression)$r.squared
+
+pred2 <- pred2$mean
+myDf <- as.data.frame(pred2)
+regression = lm(pred2 ~ Clay1+OC1, data = soil)
+summary(regression)$r.squared
 
 # Task 7
 
